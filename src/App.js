d3add4251ee32f5c032a57b380d44aa8ba64b70e
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2022-11-02 01:16:42 +0600

    add banner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from "./Pages/Shared/Header/Header";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.min';
+import Footer from "./Pages/Shared/Footer/Footer";
+import Banner from "./Pages/Home/Banner/Banner";
 
 
 function App() {
   return (
     <div>
      <Header/>
+     <Banner/>
+     <Footer/>
     </div>
   );
 }
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -5,8 +5,8 @@ const Header = () => {
     return (
         <div className=''>
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div className='container'>
-                    <a class="navbar-brand" href="#">Doctors Portal</a>
+                <div className='container px-2'>
+                    <a class="navbar-brand" href="#">Doctors Portal BD</a>
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
